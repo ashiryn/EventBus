d1634c66db392066477fd3579db0c39e176b1cad
Joe Thompson <azyle0823@gmail.com>
2025-09-11 17:15:28 -0400

    Initial commit of files for the library

--- EventBus.Tests/GlobalEventBusTests.cs
+++ EventBus.Tests/GlobalEventBusTests.cs
@@ -3,137 +3,141 @@ using NSubstitute;
 
 namespace FluffyVoid.EventBus.Tests;
 
-public class LocalEventBusTests
+public class GlobalEventBusTests
 {
-    private LocalEventBus _eventBus;
     private MockEvent _event;
     private Action<object, MockEvent> _subscriber = Substitute.For<Action<object, MockEvent>>();
 
     [SetUp]
     public void Setup()
     {
-        _eventBus = new LocalEventBus();
         _event = Substitute.For<MockEvent>();
+        int count = GlobalEventBus.Count<MockEvent>();
+        for (int i = 0; i < count; ++i)
+        {
+            GlobalEventBus.Unsubscribe(_subscriber);
+            GlobalEventBus.Unsubscribe<MockEvent>(null!);
+        }
         _subscriber.ClearReceivedCalls();
     }
 
     [Test]
     public void TestPublishWithNoSubscribers()
     {
-        Assert.DoesNotThrow(() => _eventBus.Publish(Substitute.For<object>(), Substitute.For<EventArgs>()));
+        Assert.DoesNotThrow(() => GlobalEventBus.Publish(Substitute.For<object>(), Substitute.For<EventArgs>()));
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(0));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(0));
         _subscriber.DidNotReceive().Invoke(Arg.Any<object>(), Arg.Any<MockEvent>());
     }
     [Test]
     public void TestPublishWithSingleSubscriber()
     {
-        _eventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
 
-        _eventBus.Publish(Substitute.For<object>(), _event);
+        GlobalEventBus.Publish(Substitute.For<object>(), _event);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(1));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(1));
         _subscriber.Received(1).Invoke(Arg.Any<object>(), _event);
     }
     [Test]
     public void TestPublishWithMultipleSubscribers()
     {
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
 
-        _eventBus.Publish(Substitute.For<object>(), _event);
+        GlobalEventBus.Publish(Substitute.For<object>(), _event);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(4));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(4));
         _subscriber.Received(4).Invoke(Arg.Any<object>(), _event);
     }
     [Test]
     public void TestPublishWithMultipleSubscribersWhereOneCrashes()
     {
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe((object sender, MockEvent @event) =>
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe((object sender, MockEvent @event) =>
         {
             int i = 0;
             int j = 12 / i;
         });
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
 
-        _eventBus.Publish(Substitute.For<object>(), _event);
+        GlobalEventBus.Publish(Substitute.For<object>(), _event);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(3));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(3));
         _subscriber.Received(3).Invoke(Arg.Any<object>(), _event);
     }
     [Test]
     public void TestPublishWithMultipleSubscribersWhereOneIsNull()
     {
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe<MockEvent>(null!);
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe<MockEvent>(null!);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
 
-        _eventBus.Publish(Substitute.For<object>(), _event);
+        GlobalEventBus.Publish(Substitute.For<object>(), _event);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(3));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(3));
         _subscriber.Received(3).Invoke(Arg.Any<object>(), _event);
     }
     [Test]
     public void TestPublishWithNullValues()
     {
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
 
-        _eventBus.Publish(null!, (MockEvent)null!);
+        GlobalEventBus.Publish(null!, (MockEvent)null!);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(4));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(4));
         _subscriber.Received(4).Invoke(null!, null!);
     }
     [Test]
     public void TestSubscribeWithNewEvent()
     {
-        _eventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(1));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(1));
     }
     [Test]
     public void TestSubscribeWithExistingEvent()
     {
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(2));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(2));
     }
     [Test]
     public void TestSubscribeWithNullSubscriber()
     {
-        _eventBus.Subscribe<MockEvent>(null!);
+        GlobalEventBus.Subscribe<MockEvent>(null!);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(1));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(1));
     }
     [Test]
     public void TestUnsubscribeWithNoEventSubscribed()
     {
-        _eventBus.Unsubscribe(_subscriber);
+        GlobalEventBus.Unsubscribe(_subscriber);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(0));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(0));
     }
     [Test]
     public void TestUnsubscribeWithExistingEvent()
     {
-        _eventBus.Subscribe(_subscriber);
-        _eventBus.Unsubscribe(_subscriber);
+        GlobalEventBus.Subscribe(_subscriber);
+        GlobalEventBus.Unsubscribe(_subscriber);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(0));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(0));
     }
     [Test]
     public void TestUnsubscribeWithNullSubscriber()
     {
-        _eventBus.Unsubscribe<MockEvent>(null!);
+        GlobalEventBus.Unsubscribe<MockEvent>(null!);
 
-        Assert.That(_eventBus[typeof(MockEvent)], Is.EqualTo(0));
+        Assert.That(GlobalEventBus.Count<MockEvent>(), Is.EqualTo(0));
     }
     public class MockSender
     {
